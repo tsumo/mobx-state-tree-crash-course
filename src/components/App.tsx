efc76c8bff7605b3e6731eb34e226af9e53a371b
tsumo <tsumo@users.noreply.github.com>
2020-01-13 11:36:43 +0300

    Log store snapshots

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import store from '../store'
-import { getSnapshot } from 'mobx-state-tree'
 
 store.addSpell('ct', 'Chill Touch', 10)
 store.addWizard('pu', 'Paula Undergun', 100)
-console.log(getSnapshot(store))
 
 const App: React.FC = () => {
   return (
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,8 @@
+import { onSnapshot } from 'mobx-state-tree'
 import RootStore from '../models'
 
 const store = RootStore.create()
+
+onSnapshot(store, snapshot => console.log(snapshot))
 
 export default store
