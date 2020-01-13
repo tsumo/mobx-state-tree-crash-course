cee4ef0cfaa599255a988c2b511cbeee7a8d857e
tsumo <tsumo@users.noreply.github.com>
2020-01-13 17:20:20 +0300

    Smaller font

--- src/components/Card.style.ts
+++ src/components/Card.style.ts
@@ -2,8 +2,8 @@ import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
   root: {
-    width: '200px',
-    height: '300px',
+    width: '150px',
+    height: '230px',
     border: '1.2px solid rgba(255,255,255,0.6)',
     borderRadius: '10px',
     boxShadow: '0px 0px 36px 7px rgba(255,255,255,0.2)',
@@ -11,6 +11,7 @@ const useStyles = createUseStyles({
     padding: '15px',
     margin: '30px',
     lineHeight: 2,
+    fontSize: '2em',
   },
 })
 
